ad4749460ed43fae5318b355960223c1601bd013
cjaybond <jamesuchennachi@gmail.com>
2023-01-06 11:26:35 +0100

    Developed shopping aggregates consuming services for internal microservices.

---
+++ src/ApiGateways/ShoppingAggregator/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using Shopping.Aggregator.Models;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public interface IBasketService
+    {
+        public interface IBasketService
+        {
+            Task<BasketModel> GetBasket(string userName);
+        }
+    }
+}
