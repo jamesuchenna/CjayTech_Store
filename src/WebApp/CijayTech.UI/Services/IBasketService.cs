6615e29344d4c32edb9dafed8db23738017cb838
cjaybond <jamesuchennachi@gmail.com>
2023-01-09 21:00:50 +0100

    Added services

---
+++ src/WebApp/CijayTech.UI/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using CjayTech.UI.Model;
+using System.Threading.Tasks;
+
+namespace CjayTech.UI.Services
+{
+    public interface IBasketService
+    {
+        Task<BasketModel> GetBasket(string userName);
+        Task<BasketModel> UpdateBasket(BasketModel model);
+        Task CheckoutBasket(BasketCheckoutModel model);
+    }
+}
---
+++ src/WebApp/CijayTech.UI/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using CjayTech.UI.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CjayTech.UI.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
