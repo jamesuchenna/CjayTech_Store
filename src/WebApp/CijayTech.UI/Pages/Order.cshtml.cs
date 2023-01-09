35240ca5bc20c67a676aa4049e9332d3793c9a30
cjaybond <jamesuchennachi@gmail.com>
2023-01-10 00:55:43 +0100

    Fixed order page

--- src/WebApp/CijayTech.UI/Pages/Order.cshtml.cs
+++ src/WebApp/CijayTech.UI/Pages/Order.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AspnetRunBasics.Repositories;
+using CjayTech.UI.Model;
+using CjayTech.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,20 +11,20 @@ namespace AspnetRunBasics
 {
     public class OrderModel : PageModel
     {
-        private readonly IOrderRepository _orderRepository;
+        private readonly IOrderService _orderService;
 
-        public OrderModel(IOrderRepository orderRepository)
+        public OrderModel(IOrderService orderService)
         {
-            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
         }
 
-        public IEnumerable<Entities.Order> Orders { get; set; } = new List<Entities.Order>();
+        public IEnumerable<OrderResponseModel> Orders { get; set; } = new List<OrderResponseModel>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Orders = await _orderRepository.GetOrdersByUserName("test");
+            Orders = await _orderService.GetOrdersByUserName("slek");
 
             return Page();
-        }       
+        }
     }
 }
